6730980630f73755dbae551d4c84e865117088b8
danmilldev <126679134+danmilldev@users.noreply.github.com>
2023-04-07 18:08:24 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿
+using TaskManagerSQLProject;
+
+TaskSystem task = new();
---
+++ TaskSystem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerSQLProject
+{
+    class TaskSystem
+    {
+        int NextID { get; set; }
+
+        public TaskSystem()
+        {
+            NextID = GetLastID();
+            Menu();
+        }
+
+        void Menu()
+        {
+            //displays a menu to change view alls tasks or update and remove tasks...
+        }
+
+        void GetAllTasks()
+        {
+            //displays all tasks as a structured list in the console
+        }
+
+        void CreateNewTask()
+        {
+            //creates a new entry in the table and makes a new task
+        }
+
+        void DeleteTask()
+        {
+            //deletes a task and searched it by id
+        }
+
+        void UpdateTask()
+        {
+            //update a task and search it by id
+        }
+
+        //implement it
+        int GetLastID()
+        {
+            //gets a list of ids and returns the lastid it was ending with
+            throw new NotImplementedException();
+        }
+    }
+}
